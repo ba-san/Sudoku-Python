ccc99153e69b1523b3e36b62671e55e0d00286ed
OBA DAISUKE <ohbadai_ybb_ne_jp@yahoo.co.jp>
2019-05-03 18:44:03 +0900

    0503

--- sudoku.py
+++ sudoku.py
@@ -6,43 +6,66 @@ import random
 frame = cv2.imread("./frame.jpg")
 frame = cv2.resize(frame, (360, 360))
 
-cnt = 0
-counted = []
 cell  = [[0 for i in range(9)] for j in range(9)]
-central_y = 3
-central_x = 3
 
-#for central 9 cells
-for i in range(3):
-	for j in range(3):
-		
-		while True:
-			break_check = 0
-			num = random.randrange(9)+1
-			for s in range(cnt):
-				if num == counted[s]:
-					break_check=1
-			if break_check == 0:
-				break
+
+def first_parts(position):
+	cnt = 0
+	counted = []
+	if position == 'center':
+		start_y = 3
+		start_x = 3
+	elif position == 'upright':
+		start_y = 0
+		start_x = 0
+	
+	for i in range(3):
+		for j in range(3):
 			
-		counted.append(num)
+			while True:
+				break_check = 0
+				num = random.randrange(9)+1
+				for s in range(cnt):
+					if num == counted[s]:
+						break_check=1
+				if break_check == 0:
+					break
+				
+			counted.append(num)
+			
+			cell[start_x][start_y] = num
+			start_y = start_y + 1
+			if position == 'center':
+				if start_y == 6:
+					start_y = 3
+					start_x = start_x + 1
+			elif position == 'upright':
+				if start_y == 3:
+					start_y = 0
+					start_x = start_x + 1
+				
+			cnt = cnt + 1
 		
-		cell[central_x][central_y] = num
-		central_y = central_y + 1
-		if central_y == 6:
-			central_y = 3
-			central_x = central_x + 1
-		cnt = cnt + 1
-		
+first_parts('upright')
+first_parts('center')		
+	
+def second_parts(direction):
+	
+	if direction == 'horizontal':
+		rotated_cell  = [[0 for i in range(9)] for j in range(9)]
+		for i in range(9):
+			for j in range(9):
+				rotated_cell[i][j] = cell[j][i]
 		
 	
-def second_parts():
 	for_cross = [[0 for i in range(9)] for j in range(3)]
 	for num in range(1, 9+1, 1):
 		
 		for i in range(3):
 			for j in range(3):
-				if cell[i+3][j+3] == num:
+				if direction == 'horizontal' and rotated_cell[i+3][j+3] == num:
+					column = i
+				elif direction == 'vertical' and cell[i+3][j+3] == num:
 					column = i
 					
 		for i in range(3):
@@ -100,20 +123,87 @@ def second_parts():
 	else:
 		other2columns(1, central_cnt, center, 2, right)
 	
-				
+	if direction == 'vertical':			
+		for i in range(3):
+			cell[3][6+i] = left[i]
+			cell[4][6+i] = center[i] #have problem? list index out of range. Problem should be in center and right
+			cell[5][6+i] = right[i]  #have problem?
+	elif direction == 'horizontal':
+		for i in range(3):
+			cell[6+i][3] = left[i]
+			cell[6+i][4] = center[i] #have problem?
+			cell[6+i][5] = right[i]  #have problem?
+
+
+second_parts('horizontal')
+second_parts('vertical')
+
+
+
+## for bottom right corner
+corner_cell  = [[0 for i in range(0)] for j in range(9)]
+
+for num in range(1, 9+1, 1):
+	
 	for i in range(3):
-		cell[3][6+i] = left[i]
-		cell[4][6+i] = center[i]
-		cell[5][6+i] = right[i]
-		
-	print('')
+		for j in range(3):
+			if cell[6+i][3+j] == num: #center right
+				col1 = i
+				#row1 = 3+j
+			if cell[3+i][6+j] == num: #bottom center
+				#col2 = 3+i
+				row2 = j
+				
 	for i in range(9):
-		print(for_cross[0][i], for_cross[1][i], for_cross[2][i])
+		if i != col1 and i != col1+3 and i != col1+6: #for x-cordinate
+			if i != row2*3 and i != row2*3+1 and i != row2*3+2: #for y-cordinate
+				corner_cell[i].append(num)
 		
+for i in range(9):
+	min_len = 9	
+	for j in range(9):
+		if min_len > len(corner_cell[j]):
+			min_len = len(corner_cell[j])
+			min_id = j
+	
+	corner_x = min_id%3
+	corner_y = min_id/3
+	
+	target = corner_cell[min_id][0]
+	cell[6+corner_x][6+corner_y] = target
+	
+	for j in range(9):
+		if target in corner_cell[j]:
+			corner_cell[j].remove(target)
+			
+	corner_cell[min_id] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	
+
+
+for_two_cells  = [[0 for i in range(0)] for j in range(6)]
+
+	
+for i in range(6):
+	
+	for num in range(1, 1+9, 1):
+		for j in range(6):
+			if cell[3+i][3+j] == num:
+				break
+		else:
+			for_two_cells[i].append(num)
+
+print(for_two_cells[0])
+print(for_two_cells[1])
+print(for_two_cells[2])
+print(for_two_cells[3])
+print(for_two_cells[4])
+print(for_two_cells[5])
 
 
 
-second_parts()
+
+
+
 	
 #show image
 for i in range(13, 364, 39):
---
+++ test.py
@@ -0,0 +1,6 @@
+
+cell  = [[0 for i in range(0)] for j in range(9)]
+
+test = []
+
+print(cell[3])
